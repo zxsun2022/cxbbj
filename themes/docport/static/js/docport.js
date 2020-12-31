cb8cfea417aef3cfa7540fb2d8acefcb02a8ca39
Zuokun <zuokun@gmai.com>
2020-12-31 14:09:22 -0800

    https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/ impl

--- themes/docport/static/js/docport.js
+++ themes/docport/static/js/docport.js
@@ -41,84 +41,26 @@ jQuery(document).ready(function() {
 
 });
 
+//---------------------
+window.addEventListener('DOMContentLoaded', () => {
 
-(function scrollSpy() {
-  var OFFSET = 10;
-  var timer;
-  var headingsCache;
-
-  var findHeadings = function findHeadings() {
-    return headingsCache || document.querySelectorAll('.TableOfContents li > a');
-  };
-
-  var onScroll = function onScroll() {
-    if (timer) {
-      // throttle
-      return;
-    }
-
-    timer = setTimeout(function() {
-      timer = null;
-      var activeNavFound = false;
-      var headings = findHeadings(); // toc nav anchors
-
-      /**
-       * On every call, try to find header right after  <-- next header
-       * the one whose content is on the current screen <-- highlight this
-       */
-
-      for (var i = 0; i < headings.length; i++) {
-        // headings[i] is current element
-        // if an element is already active, then current element is not active
-        // if no element is already active, then current element is active
-        var currNavActive = !activeNavFound;
-        /**
-         * Enter the following check up only when an active nav header is not yet found
-         * Then, check the bounding rectangle of the next header
-         * The headers that are scrolled passed will have negative bounding rect top
-         * So the first one with positive bounding rect top will be the nearest next header
-         */
-
-        if (currNavActive && i < headings.length - 1) {
-          var heading = headings[i + 1];
-          var next = decodeURIComponent(heading.href.split('#')[1]);
-          var nextHeader = document.getElementById(next);
-
-          if (nextHeader) {
-            var top = nextHeader.getBoundingClientRect().top;
-            currNavActive = top > OFFSET;
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          const id = entry.target.getAttribute('id');
+          if (entry.intersectionRatio > 0) {
+              document.querySelector(`.TableOfContents li > a[href="#${id}"]`).parentElement.classList.add('active');
           } else {
-            console.error('Can not find header element', {
-              id: next,
-              heading: heading,
-            });
+              document.querySelector(`.TableOfContents li > a[href="#${id}"]`).parentElement.classList.remove('active');
           }
-        }
-        /**
-         * Stop searching once a first such header is found,
-         * this makes sure the highlighted header is the most current one
-         */
-
-        if (currNavActive) {
-          activeNavFound = true;
-          headings[i].parentElement.classList.add('active');
-        } else {
-          headings[i].parentElement.classList.remove('active');
-        }
-      }
-    }, 100);
-  };
-
-  document.addEventListener('scroll', onScroll);
-  document.addEventListener('resize', onScroll);
-  document.addEventListener('DOMContentLoaded', function() {
-    // Cache the headings once the page has fully loaded.
-    headingsCache = findHeadings();
-    onScroll();
+      });
   });
-})();
 
-
+  // Track all sections that have an `id` applied
+  document.querySelectorAll('article section.page div.content a[id]').forEach((section) => {
+      observer.observe(section);
+  });
+  
+});
 
 
 // Get Parameters from some url
